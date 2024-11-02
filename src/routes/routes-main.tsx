72e7e26d5371b88cc9f0b3712408793c67df3a1b
capellinx <capellaaa7@gmail.com>
2024-11-02 01:40:56 -0300

    feat: create new routes

--- src/routes/routes-main.tsx
+++ src/routes/routes-main.tsx
@@ -1,3 +1,5 @@
+import CandidateDetailsPage from "@/app/candidate-details";
+import CandidateListPage from "@/app/candidate-list";
 import ForgotPasswordPage from "@/app/forgot-password";
 import LoginPage from "@/app/login";
 import Page404 from "@/app/page-404";
@@ -6,7 +8,9 @@ import { Route, Routes } from "react-router-dom";
 export const RoutesMain = () => {
    return (
       <Routes>
-         <Route path="/login" element={<LoginPage />} />
+         <Route path="/" element={<LoginPage />} />
+         <Route path="/candidate-list" element={<CandidateListPage />} />
+         <Route path="/candidate-details/:id" element={<CandidateDetailsPage />} />
          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
          <Route path="*" element={<Page404 />} />
       </Routes>
