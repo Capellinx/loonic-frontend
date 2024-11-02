3a5c0ced4a35e2035919ae611b0af46c42151f25
capellinx <capellaaa7@gmail.com>
2024-11-02 02:07:22 -0300

    feat: create new page register

--- src/routes/routes-main.tsx
+++ src/routes/routes-main.tsx
@@ -3,12 +3,14 @@ import CandidateListPage from "@/app/candidate-list";
 import ForgotPasswordPage from "@/app/forgot-password";
 import LoginPage from "@/app/login";
 import Page404 from "@/app/page-404";
+import RegisterPage from "@/app/register";
 import { Route, Routes } from "react-router-dom";
 
 export const RoutesMain = () => {
    return (
       <Routes>
          <Route path="/" element={<LoginPage />} />
+         <Route path="/register" element={<RegisterPage />} />
          <Route path="/candidate-list" element={<CandidateListPage />} />
          <Route path="/candidate-details/:id" element={<CandidateDetailsPage />} />
          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
