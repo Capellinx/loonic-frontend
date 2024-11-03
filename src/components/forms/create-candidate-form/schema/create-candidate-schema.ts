c196418966a874633366c5509332c88ef70c0ccc
capellinx <capellaaa7@gmail.com>
2024-11-03 03:07:19 -0300

    feat: create schema to validate body to invite api

--- src/components/forms/create-candidate-form/schema/create-candidate-schema.ts
+++ src/components/forms/create-candidate-form/schema/create-candidate-schema.ts
@@ -18,8 +18,12 @@ export const createCandidateSchema = z.object({
       .string({ required_error: "Experiência é obrigatória." })
       .min(1, "Experiência é obrigatória."),
    status: z
-      .enum(['DISPONIVEL', 'EM_PROCESSO', 'INDISPONIVEL'],{ required_error: "Status é obrigatório." }),
-   skills: z
-      .string({ required_error: "Habilidades são obrigatórias." })
-      .min(1, "Habilidades são obrigatórias."),
+      .enum(['DISPONIVEL', 'EM_PROCESSO', 'INDISPONIVEL'], { required_error: "Status é obrigatório." }),
+   skills: z.array(
+      z.object({
+         name: z
+            .string({ required_error: "Habilidade é obrigatória." })
+            .min(1, "Habilidade é obrigatória."),
+      })
+   ).min(1, "Pelo menos uma habilidade é obrigatória."),
 })
