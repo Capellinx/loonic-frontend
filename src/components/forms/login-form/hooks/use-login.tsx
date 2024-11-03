0a5a6f20b0aa79006df46bcbd0edf4ec7c983afb
capellinx <capellaaa7@gmail.com>
2024-11-03 01:21:39 -0300

    feat: create connection with api

--- src/components/forms/login-form/hooks/use-login.tsx
+++ src/components/forms/login-form/hooks/use-login.tsx
@@ -1,16 +1,18 @@
 import { loginSchema } from "@/components/forms/login-form/schema/login-schema";
+import { api } from "@/service/api";
 import { useMutation } from "@tanstack/react-query"
+import { AxiosError } from "axios";
 import toast from "react-hot-toast";
 import { z } from "zod"
 
-export function useLogin () {
+export function useLogin() {
    const toastId = 'register-toast'
 
-   const {mutate} = useMutation({
+   const { mutate } = useMutation({
       mutationKey: ["login-user"],
-      mutationFn: async ({email, password}: z.infer<typeof loginSchema>) => {
+      mutationFn: async ({ email, password }: z.infer<typeof loginSchema>) => {
          await submitLogin({
-            email, 
+            email,
             password
          })
       },
@@ -32,11 +34,19 @@ export function useLogin () {
    })
 
 
-   async function submitLogin({email, password}: z.infer<typeof loginSchema>) {
-      console.log({
-         email,
-         password
-      });
+   async function submitLogin({ email, password }: z.infer<typeof loginSchema>) {
+      try {
+         const { data } = await api.post("/manager/login", {
+            email,
+            password
+         })
+
+         return data
+      } catch (error) {
+         if (error instanceof AxiosError) {
+            throw new Error(error.response?.data.error)
+         }
+      }
    }
    return {
       handleLogin: mutate
