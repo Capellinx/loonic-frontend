a742bec8554bd45567d2c4c6d8fcdb026c3711cd
capellinx <capellaaa7@gmail.com>
2024-11-03 01:12:06 -0300

    feat: integrate hook with api

--- src/components/forms/register-form/hooks/use-register-form.tsx
+++ src/components/forms/register-form/hooks/use-register-form.tsx
@@ -2,15 +2,19 @@ import { useMutation } from "@tanstack/react-query"
 import { z } from "zod"
 import { registerSchema } from "../schema/register-schem"
 import toast from "react-hot-toast"
+import { api } from "@/service/api"
+import { useNavigate } from "react-router-dom"
+import { AxiosError } from "axios"
 
 
 export function useRegister() {
    const toastId = 'register-toast'
+   const navigate = useNavigate()
 
-   const { mutate } = useMutation({
+   const { mutate, error } = useMutation({
       mutationKey: ["register-form"],
       mutationFn: async ({ email, password, confirmPassword }: z.infer<typeof registerSchema>) => {
-         submitRegister({
+         await submitRegister({
             email,
             password,
             confirmPassword
@@ -22,6 +26,7 @@ export function useRegister() {
          })
       },
       onError: (err) => {
+         console.log(err.message);
          toast.error(err.message, {
             id: toastId
          })
@@ -30,19 +35,25 @@ export function useRegister() {
          toast.success("Registro realizado com sucesso!", {
             id: toastId
          })
+         navigate("/")
       },
    })
 
 
-   function submitRegister(payload: z.infer<typeof registerSchema>) {
-      const { email, password } = payload
-      console.log({
-         email,
-         password
-      });
+   async function submitRegister(payload: z.infer<typeof registerSchema>) {
+      try {
+         const { data } = await api.post("/manager", payload)
+
+         return data
+      } catch (error) {
+         if (error instanceof AxiosError) {
+            throw new Error(error.response?.data.error)
+         }
+      }
    }
 
    return {
-      handleRegister: mutate
+      handleRegister: mutate,
+      error
    }
 } 
