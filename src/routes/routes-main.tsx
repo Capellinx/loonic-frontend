fc67de4688d00e89d23768fcbb69171a9036dd0b
capellinx <capellaaa7@gmail.com>
2024-11-03 02:01:11 -0300

    feat: create protected routes

--- src/routes/routes-main.tsx
+++ src/routes/routes-main.tsx
@@ -4,6 +4,7 @@ import ForgotPasswordPage from "@/app/forgot-password";
 import LoginPage from "@/app/login";
 import Page404 from "@/app/page-404";
 import RegisterPage from "@/app/register";
+import { AuthControllerAccess } from "@/components/auth-controller-access/auth-controller-access";
 import { Route, Routes } from "react-router-dom";
 
 export const RoutesMain = () => {
@@ -11,8 +12,14 @@ export const RoutesMain = () => {
       <Routes>
          <Route path="/" element={<LoginPage />} />
          <Route path="/register" element={<RegisterPage />} />
-         <Route path="/candidate-list" element={<CandidateListPage />} />
-         <Route path="/candidate-details/:id" element={<CandidateDetailsPage />} />
+         
+         // protected routes
+         <Route element={<AuthControllerAccess/>}>
+            <Route path="/candidate-list" element={<CandidateListPage />} />
+            <Route path="/candidate-details/:id" element={<CandidateDetailsPage />} />
+         </Route>
+         
+    
          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
          <Route path="*" element={<Page404 />} />
       </Routes>
